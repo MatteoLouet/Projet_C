c9f417c7180db0230af582c75788abdf48290480
AnthimeLh <124272287+AnthimeLh@users.noreply.github.com>
2024-05-20 22:48:37 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,23 +1,43 @@
 //
 // Created by anthi on 21/04/2024.
 //
+#include "cdataframe.c"
 
-#include "column.c"
-#include <stdlib.h>
-#include <stdio.h>
+int z = 0 ;
+char titre_col[] = "col_";
+char num[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
 
 int main() {
 
-    COLUMN *mycol = create_column("My column");
-    insert_value(mycol, 5);
-    insert_value(mycol, 10);
-    insert_value(mycol, 15);
-    print_col(mycol);
-    nbr_occu(mycol, 10);
-    position(mycol, 1);
-    nbr_sup(mycol, 5);
-    nbr_inf(mycol, 10);
-    delete_column(mycol);
+
+    COLUMN *col = create_column("My column");
+    insert_value(col, 5);
+    insert_value(col, 10);
+    insert_value(col, 15);
+    print_col(col);
+    nbr_occu(col, 10);
+    position(col, 1);
+    nbr_sup(col, 5);
+    nbr_inf(col, 10);
+    delete_column(col);
+
+    CDATAFRAME *lig = create_cdataframe();
+    printf("Cb de colonne voulez-vous ? : ");
+    scanf("%d",&temp);
+    printf("Combien de valeurs voulez-vous ajouter ? : ");
+    scanf("%d", &nbr);
+    while (temp != 0)
+    {
+        titre_col[4] = num[z];
+        z++;
+        remlissge_dur_dataframe(lig,titre_col,nbr);
+        temp --;
+    }
+
+    affiche_dataframe(lig);
+
+
 
 
     return 0;
